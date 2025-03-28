77b2c5b097dc232acae72663b8e801656beefc76
vovatrykoz <vovatrykoz@gmail.com>
2025-03-28 17:54:47 +0100

    update example and target current .net LTS

--- test.Console/Program.cs
+++ test.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Mathematics;
+
+Quaternion quat = Quaternion.Zero;
+
+Vector3 vec3 = Vector3.One;
+
+var example1 = quat * vec3;
+
+// Uncommenting this results in CS0019: "Operator '*' cannot be applied to operands of type 'Vector3' and 'Quaternion'"
+// var example2 = vec3 * quat;
+
+Matrix3 mat3 = Matrix3.Identity;
+
+var example3 = mat3 * vec3;
+var example4 = vec3 * mat3;
