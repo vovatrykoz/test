e95b91145bfccbb3d83f2507054528a729d03139
vovatrykoz <vovatrykoz@gmail.com>
2025-04-06 17:08:09 +0200

    pull latest changes

--- test.Console/Program.cs
+++ test.Console/Program.cs
@@ -14,22 +14,8 @@ Matrix3 mat3 = Matrix3.Identity;
 var example3 = mat3 * vec3; // resolves to Vector3 Matrix3.operator *(Matrix3 mat, Vector3 vec), defined in Matrix3.cs
 var example4 = vec3 * mat3; // resolves to Vector3 Vector3.operator *(Vector3 vec, Matrix3 mat), defined in Vector3.cs
 
+var left = new Matrix4x3(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+var right = new Matrix3(13, 14, 15, 16, 17, 18, 19, 20, 21);
+var result = left * right;
 
-
-// Identity matrices
-
-var mat3x2 = new Matrix3x2(new Vector2(1, 2), new Vector2(3, 4), new Vector2(5, 6));
-var identityMultiplicationResult3x2 = Matrix3x2.MultiplicativeIdentity * mat3x2;
-
-// The reversed order doesn't work due to the matrix multiplication rules
-// var identityMultiplicationResult3x2 = mat3x2 * Matrix3x2.MultiplicativeIdentity;
-
-Console.WriteLine($"Identity matrix\n{Matrix3x2.MultiplicativeIdentity}\nmultiplied by a 3x2 matrix:\n{mat3x2}\n\nIdentity multiplication result:\n{identityMultiplicationResult3x2}\n\n");
-
-var mat2x3 = new Matrix2x3(new Vector3(1, 2, 3), new Vector3(4, 5, 6));
-var identityMultiplicationResult2x3 = Matrix2x3.MultiplicativeIdentity * mat2x3;
-
-// The reversed order doesn't work due to the matrix multiplication rules
-// var identityMultiplicationResult3x2 = mat2x3 * Matrix2x3.MultiplicativeIdentity;
-
-Console.WriteLine($"Identity matrix\n{Matrix2x3.MultiplicativeIdentity}\nmultiplied by a 2x3 matrix:\n{mat2x3}\n\nIdentity multiplication result:\n{identityMultiplicationResult2x3}");
+Console.WriteLine($"Left:\n{left}\nRight:\n{right}\n\nResult:\n{result}");
